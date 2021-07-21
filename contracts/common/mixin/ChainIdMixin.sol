99df45b196b5bfe8eadc5379046d189814e16888
Mike Grant <mgrant@circle.com>
2021-07-21 18:45:57 -0400

    fix

--- contracts/common/mixin/ChainIdMixin.sol
+++ contracts/common/mixin/ChainIdMixin.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.2;
 
 contract ChainIdMixin {
-  bytes constant public networkId = hex"15001";
+  bytes constant public networkId = hex"01";
   uint256 constant public CHAINID = 15001;
 }
