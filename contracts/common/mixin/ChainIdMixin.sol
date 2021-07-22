7d0552620bfa3f78b1c735a27c7c5e0671bafb17
Mike Grant <mgrant@circle.com>
2021-07-22 12:02:21 -0400

    use correct network id

--- contracts/common/mixin/ChainIdMixin.sol
+++ contracts/common/mixin/ChainIdMixin.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.5.2;
 
 contract ChainIdMixin {
-  bytes constant public networkId = hex"01";
+  // networkId = * to match any network
+  bytes constant public networkId = hex"2a";
   uint256 constant public CHAINID = 15001;
 }
